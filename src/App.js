b8c68a6e35dff3a482c8f9edbd4a74c5d3ee944d
niiholmes <iamniiholmes@gmail.com>
2021-03-12 20:06:26 +0000

    todo taking shape

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-
+import React,{useState} from 'react'
+import Todo from './Components/Todo'
+import Form from './Components/Form'
+import FilterButton from './Components/FilterButton'
 import './App.css';
 
-function App() {
+function App(props) {
 
-  return(
-    <div>
+  const[task, setTask]= useState(props.tasks)
 
-      
+  const addTask = (name) => {
+    alert(name)
+  }
+
+  const taskList = props.tasks.map(task =>  (
+
+  <Todo
+   id ={task.id} 
+   name={task.name} 
+   completed={task.completed}
+   key={task.id}
+   />
+
+  )
+   
+   )
+
+  return (
+    <div className="todoapp stack-large">
+
+      <h1>GirlFriday</h1>
+
+      <Form addTask={addTask}/>
+
+      <div className="filters btn-group stack-exception">
+      <FilterButton/>
+      <FilterButton/>
+      <FilterButton/>
+      </div>
+
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+
     </div>
+
   );
 }
 
---
+++ src/Components/FilterButton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const FilterButton = (props) => {
+    return(
+        <div>
+          <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        </div>
+    )
+}
+
+
+
+export default FilterButton
