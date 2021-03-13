210c2740fd7eaccf8a496b238127f49b6815bcb0
niiholmes <iamniiholmes@gmail.com>
2021-03-13 19:49:23 +0000

    another one

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import FilterButton from './Components/FilterButton'
 import './App.css';
 import {nanoid} from 'nanoid'
 
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 function App(props) {
 
   const[tasks, setTasks]= useState(props.tasks)
@@ -57,6 +66,15 @@ function App(props) {
   )
    
    )
+
+   const filterList = FILTER_NAMES.map(name => (
+     <FilterButton 
+     key ={name} 
+     name = {name} 
+     isPressed = {name === filter}
+     setFilter = {setFilter}
+     />
+   ))
    
    const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'
    const headingText = `${taskList.length} tasks remaining`
@@ -69,9 +87,7 @@ function App(props) {
       <Form addTask={addTask}/>
 
       <div className="filters btn-group stack-exception">
-      <FilterButton/>
-      <FilterButton/>
-      <FilterButton/>
+      {filterList}
       </div>
 
       <h2 id="list-heading">
